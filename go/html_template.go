da207fd460513875c3607fd90c61317026bfa190
Hoanh An <hoanhan@bennington.edu>
2018-06-08 13:42:45 -0400

    finish html template

--- go/html_template.go
+++ go/html_template.go
@@ -10,12 +10,29 @@ type Gopher struct {
 	Name string
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    log.Print("/hello-gopher")
+
 	var gophername string
 	gophername = r.URL.Query().Get("gophername")
 	if gophername == "" {
 		gophername = "Gopher"
 	}
 	gopher := Gopher{Name: gophername}
-	re
+	renderTemplate(w, "./templates/sample.html", gopher)
+}
+
+func renderTemplate(w http.ResponseWriter, templateFile string, templateData interface{}) {
+    t, err := template.ParseFiles(templateFile)
+    if err != nil {
+        log.Fatal("Error encountered while parsing the template: ", err)
+    }
+    t.Execute(w, templateData)
+}
+
+func main() {
+    log.Print("Server is running at port 8080")
+
+    http.HandleFunc("/hello-gopher", helloHandler)
+    http.ListenAndServe(":8080", nil)
 }
