571bca04ba397a9bcbf4d69fbb698bc721bad44c
Hoanh An <hoanhan@bennington.edu>
2018-06-08 15:28:26 -0400

    gofmt

--- go/text_template.go
+++ go/text_template.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"os"
 	"text/template"
-    "os"
 )
 
 type Inventory struct {
@@ -13,13 +13,13 @@ type Inventory struct {
 func main() {
 	sweaters := Inventory{Material: "wood", Count: 17}
 
-    tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
-    if err != nil {
-        panic(err)
-    }
+	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+	if err != nil {
+		panic(err)
+	}
 
-    err = tmpl.Execute(os.Stdout, sweaters)
-    if err != nil {
-        panic(err)
-    }
+	err = tmpl.Execute(os.Stdout, sweaters)
+	if err != nil {
+		panic(err)
+	}
 }
